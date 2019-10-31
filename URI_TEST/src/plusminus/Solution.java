17407a8418af0b881f5b8da0155a268e772bdab2
Rachel Bittar <rachelbbittar@gmail.com>
2019-10-30 22:28:28 -0400

    ok

---
+++ URI_TEST/src/plusminus/Solution.java
@@ -0,0 +1,69 @@
+package plusminus;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the plusMinus function below.
+    static void plusMinus(int[] arr) {
+    	
+    	DecimalFormat df2 = new DecimalFormat("0.000000");
+    	
+    	double plus  = 0; 
+    	double minus = 0; 
+    	double zero=0;
+
+    	for(int i=0; i<arr.length;i++) {
+    		
+    		if(arr[i]>0) {
+    			plus++;
+    			
+    		}
+    		if(arr[i]<0) {
+    			minus++;			
+    		}
+    		
+    		if(arr[i]==0) {
+    			zero++;
+    		  			
+    		}
+    	}
+    	
+    	
+    	System.out.print(df2.format(plus/arr.length) + " ");
+    	System.out.print(df2.format(minus/arr.length)+ " ");
+    	System.out.print(df2.format(zero/arr.length));
+    
+    	
+    	
+  	
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        plusMinus(arr);
+
+        scanner.close();
+    }
+}
