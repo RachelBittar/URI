3f88770ebca81a17074899c3ec91a3c048d0c5b0
Rachel Bittar <rachelbbittar@gmail.com>
2019-11-14 18:12:58 -0500

    ok

---
+++ URI_TEST/src/MoveZero/Solution.java
@@ -0,0 +1,34 @@
+package MoveZero;
+
+import java.util.Arrays;
+
+public class Solution {
+
+	public static void main(String[] args) {
+	
+		int[] nums =  {0,1,0,3,12};
+		moveZeroes(nums);
+	}
+
+	private static void moveZeroes(int[] nums) {
+		
+		int index=0;
+		int aux=0;
+		
+		for(int i=0; i<nums.length; i++) {
+					
+				if(nums[i]!=0) {
+					nums[index++] = nums[i];
+		   }
+		}
+		
+		for(int i=index; i<nums.length; i++) {	
+				nums[i] = 0;
+		
+	   }
+		
+		System.out.println(Arrays.toString(nums));
+	}
+	
+
+}
